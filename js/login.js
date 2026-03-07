1f5efd956859f10cbff491b3e392e11732d5bc1c
rashedulislam595 <rashedulislam956581@gmail.com>
2026-03-07 11:28:28 +0600

    set login condition & going to main page

---
+++ js/login.js
@@ -0,0 +1,31 @@
+
+document.getElementById("sign-in-btn").addEventListener("click",function(){
+    // get username
+    const usernameInput = document.getElementById("username-input");
+    const username = usernameInput.value;
+    // validation
+    if(username == ""){
+        alert("Please show your user name!!");
+        return;
+    }
+
+    // get password 
+    const passwordInput = document.getElementById("password-input");
+    const password = passwordInput.value;
+    // validation
+    if(password == ""){
+        alert("Please show your password");
+        return;
+    }
+
+    // login condition
+    if(username == "admin" && password == "admin123"){
+        window.location.assign("../mainPage.html");
+    }else{
+        alert("Wrong username or password !! Try again");
+    }
+    usernameInput.value = "";
+    passwordInput.value = "";
+    
+    
+})
