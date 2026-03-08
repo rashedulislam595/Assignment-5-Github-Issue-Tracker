4489767e123b5c4dc51590bf466f105549146284
rashedulislam595 <rashedulislam956581@gmail.com>
2026-03-08 23:09:18 +0600

    update search  Functionality

--- js/main.js
+++ js/main.js
@@ -175,6 +175,7 @@ searchIssue.addEventListener("click",async()=>{
     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${search}`)
     const data = await res.json();
     displayIssue(data.data)
+    searchInput.value = "";
 })
 
 
